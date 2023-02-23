2d41701d633c8dd0023aec2086442f9339475e98
Steve Amos <71890451+samos-saic@users.noreply.github.com>
2023-02-22 23:41:08 -0600

    Create basic project files. First iteration
    First, a p5.js sketch is created with a setup() function that uses the createCanvas() function to create a canvas. The canvas size is set to 600x600 pixels in this example.
    
    Next, a createGrid() function is defined to take two arguments: numRows and numCols, which indicate the number of rows and columns in the grid.
    
    Inside the createGrid() function, the width and height of each cell in the grid are calculated based on the canvas size and the number of rows and columns.
    
    An array of colors is created to use with the grid. A gradient of six colors is used in this example, but the number of colors can be adjusted.
    
    A nested for loop is used to iterate over each cell in the grid. For each cell, its x and y position is calculated based on its row and column index. Then, Perlin noise is used to generate a random color value for the cell. The Perlin noise value is mapped to an index in the colors array to select a fill color for the cell.
    
    Finally, the createGrid() function is called from the draw() function of the p5.js sketch. A Perlin noise animation is added by incrementing the noise value over time.
    
    Optionally, CSS can be added to center the canvas in the browser window. This is achieved by setting the position to absolute and using the top, left, and transform properties to center the canvas within its parent container.

---
+++ sketch.js
@@ -0,0 +1,54 @@
+let noiseVal = 0; // starting noise value
+let noiseIncrement = 0.01; // increment value for noise animation
+
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  createGrid(40, 40);
+  //update the noiseVal variable over time
+  noiseVal += noiseIncrement;
+}
+
+function createGrid(numRows, numCols) {
+
+  // calculate cell size based on canvas dimensions and number of rows and columns
+  let cellWidth = width / numCols;
+  let cellHeight = height / numRows;
+  
+  // define a gradient of colors to use with Perlin noise
+  let colors = [
+    color(255, 0, 0),
+    color(255, 255, 0),
+    color(0, 255, 0),
+    color(0, 255, 255),
+    color(0, 0, 255),
+    color(255, 0, 255),
+    //color(128, 128, 128),
+    color(255, 128, 0),
+    //color(0, 128, 255)
+  ];
+   
+  // nested for loop to create cells
+  for (let row = 0; row < numRows; row++) {
+    for (let col = 0; col < numCols; col++) {
+      // calculate x and y positions for each cell
+      let x = col * cellWidth;
+      let y = row * cellHeight;
+      
+      // calculate a Perlin noise value based on the cell's x and y coordinates
+      let cellNoiseVal = noise(x / 100, y / 100, noiseVal);
+      
+      // map the noise value to an index in the colors array
+      let colorIndex = Math.floor(map(cellNoiseVal, 0, 1, 0, colors.length));
+      let cellColor = colors[colorIndex];
+      
+      // set fill color for this cell
+      fill(cellColor);
+      
+      // draw cell
+      rect(x, y, cellWidth, cellHeight);
+    }
+  }
+}
